9bdeabbbef6c7d41c9faec5bc291e2a1b5e697c0
CPrzMtz <112624260+CPrzMtz@users.noreply.github.com>
2022-10-27 18:08:48 -0500

    restart

--- frontend/src/Components/Section.js
+++
@@ -1,8 +0,0 @@
-export const Section = ({name, children}) => {
-    return(
-        <>
-        <div className="section-title">{name}</div>
-        <div className="section-info">{children}</div>
-        </>
-    );
-}
